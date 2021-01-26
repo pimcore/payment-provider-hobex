3bd4d413d6af8dfd9347903cabf6fac0a7ed4834
dvesh3 <divesh.pahuja@pimcore.com>
2021-01-26 19:22:03 +0100

    initial commit

---
+++ src/Hobex/Installer.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Pimcore
+ *
+ * This source file is available under following license:
+ * - Pimcore Enterprise License (PEL)
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     PEL
+ */
+namespace Pimcore\Bundle\EcommerceFrameworkBundle\Hobex;
+
+use Pimcore\Bundle\EcommerceFrameworkBundle\Tools\PaymentProviderInstaller;
+
+class Installer extends PaymentProviderInstaller
+{
+    protected $bricksToInstall = [
+        'PaymentProviderDatatrans' => 'objectbrick_PaymentProviderOGone_export.json'
+    ];
+}
